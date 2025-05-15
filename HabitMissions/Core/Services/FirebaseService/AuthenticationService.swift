c34701b05544c8811b763321f01cb92bc1e193c5
davidspringean12 <springeandavid5@gmail.com>
2025-05-15 19:29:17 +0200

    add Firebase database and Auth

---
+++ HabitMissions/Core/Services/FirebaseService/AuthenticationService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationService.swift
+//  HabitMissions
+//
+//  Created by David Springean on 15.05.25.
+//
+
+import Foundation
---
+++ HabitMissions/Core/Services/FirebaseService/FirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseManager.swift
+//  HabitMissions
+//
+//  Created by David Springean on 15.05.25.
+//
+
+import Foundation
