1353acf2b7e3b7e728826955866c4b86be017b7a
davidspringean12 <springeandavid5@gmail.com>
2025-05-15 16:49:31 +0200

    Initial Commit

---
+++ HabitMissions/HabitMissionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMissionsApp.swift
+//  HabitMissions
+//
+//  Created by David Springean on 15.05.25.
+//
+
+import SwiftUI
+
+@main
+struct HabitMissionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitMissionsTests/HabitMissionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMissionsTests.swift
+//  HabitMissionsTests
+//
+//  Created by David Springean on 15.05.25.
+//
+
+import Testing
+@testable import HabitMissions
+
+struct HabitMissionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
